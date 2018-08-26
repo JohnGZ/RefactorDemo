a9d49ec71738f19d8bddad9eee169c0bb193df09
JohnGao <john.gao@oocl.com>
2018-08-26 21:19:41 +0800

    将写死的字符串封装到一个CONST类中

---
+++ src/main/java/rentalstore/CONST.java
@@ -0,0 +1,13 @@
+package rentalstore;
+
+public class CONST {
+    public static final String NAME_LINE_BEGIN = "<H1>Rental Record for <EM>";
+    public static final String NAME_LINE_END = "</EM></H1><P>\n";
+    public static final String RECORD_BEGIN = "\t";
+    public static final String EQUAL = ":";
+    public static final String RECORD_END = "<BR>\n";
+    public static final String OWE_LINE_BEGIN = "<P>You owe<EM>";
+    public static final String OWE_LINE_END = "</EM><P>\n";
+    public static final String EARNED_LINE_BEGIN = "On this rental you earned <EM>";
+    public static final String EARNED_LINE_END = "</EM> frequent renter points<P>";
+}
--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -7,6 +7,27 @@ public class Customer {
     private String name;
     private Vector rentals = new Vector();
 
+    public String statement(){
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = this.rentals.elements();
+        String result = CONST.NAME_LINE_BEGIN + getName() + CONST.NAME_LINE_END;
+        while(rentals.hasMoreElements()){
+            double thisAmount;
+            Rental each = (Rental) rentals.nextElement();
+            thisAmount = each.getMovie().getAmount(each.getDayRented());
+            frequentRenterPoints += each.getMovie().getPoint();
+            //show figures for this rental
+            result += CONST.RECORD_BEGIN + each.getMovie().getTitle() + CONST.EQUAL + String.valueOf(thisAmount) + CONST.RECORD_END;
+            totalAmount += thisAmount;
+        }
+
+        //add footer lines
+        result += CONST.OWE_LINE_BEGIN + String.valueOf(totalAmount) + CONST.OWE_LINE_END;
+        result += CONST.EARNED_LINE_BEGIN + String.valueOf(frequentRenterPoints) + CONST.EARNED_LINE_END;
+        return result;
+    }
+
     public Customer(String name) {
         this.name = name;
     }
@@ -17,26 +38,5 @@ public class Customer {
 
     public String getName() {
         return name;
-    }
-
-    public String statement(){
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
-        String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><P>\n";
-        while(rentals.hasMoreElements()){
-            double thisAmount =0;
-            Rental each = (Rental) rentals.nextElement();
-            thisAmount = each.getMovie().getAmount(each.getDayRented());
-            frequentRenterPoints += each.getMovie().getPoint();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + ":" + String.valueOf(thisAmount) + "<BR>\n";
-            totalAmount += thisAmount;
-        }
-
-        //add footer lines
-        result += "<P>You owe<EM>" + String.valueOf(totalAmount) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + String.valueOf(frequentRenterPoints) + "</EM> frequent renter points<P>";
-        return result;
     }
 }
